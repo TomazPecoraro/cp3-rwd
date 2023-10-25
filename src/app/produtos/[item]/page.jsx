29fcb8e4980178a619c5cc2a2888905483cac26b
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-10-25 17:10:17 -0300

    Rotas

--- src/app/produtos/[item]/page.jsx
+++ src/app/produtos/[item]/page.jsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
-
 export default async function ProdutoID({params}) {
 
+  const response = await fetch("http://localhost:3000/dados/produto-api");
+  const produtos = await response.json();
+
+  const produto = produtos.find(produto => produto.id == params.id);
 
 
-    const response = await fetch("http://localhost:3000/dados/produto-api");
-    const produtos = await response.json();
-  
-    const produto = produtos.find(produto => produto.item == params.item);
-  
-  
-    return (
-      <div>
-          <h1>Identificação dos Itens</h1>
-          <h3>Nome do Item : {params.item}</h3>
-          <Image src= {params.Image} alt="Imagem" width={750} height={600}/>
-      </div>
-    )
-  }
+  return (
+    <div>
+        <h1>Identificação das Produtos</h1>
+        <p>Valor do ID : {params.id}</p>
+        <p>Nome do produto: {produto.nome}</p>
+        <p>Descrição do produto: {produto.desc}</p>
+        <Image src= {params.Image} alt="Imagem" width={750} height={600}/>
+
+    </div>
+  )
+}
