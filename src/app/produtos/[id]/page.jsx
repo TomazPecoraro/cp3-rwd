cbefd44f652dd118e19d05df44cff35e31464889
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-10-26 23:33:29 -0300

    Correção

--- src/app/produtos/[id]/page.jsx
+++ src/app/produtos/[id]/page.jsx
@@ -1,20 +1,18 @@
 import Image from "next/image";
-export default async function ProdutoID({params}) {
 
+export default async function ProdutoID({ params }) {
   const response = await fetch("http://localhost:3000/dados/produto-api");
   const produtos = await response.json();
 
-  const produto = produtos.find(produto => produto.id == params.id);
-
+  const produto = produtos.find((produto) => produto.id == params.id);
 
   return (
     <div>
-        <h1>Identificação das Produtos</h1>
-        <p>Valor do ID: {params.id}</p>
-        <p>Nome do produto: {produto.item}</p>
-        <p>Descrição do produto: {produto.desc}</p>
-        <Image src= {params.Image} alt="Imagem" width={750} height={600}/>
-
+      <h1>Identificação das Produtos</h1>
+      <p>Valor do ID: {params.id}</p>
+      <p>Nome do produto: {produto.item}</p>
+      <p>Descrição do produto: {produto.desc}</p>
+      <Image src={params.imagem} alt="Imagem" width={750} height={600} />
     </div>
-  )
+  );
 }
